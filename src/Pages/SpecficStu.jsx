873afbd87339159809b4f199455d9782be04da64
sami855-ux <samitale56@outlook.com>
2024-09-13 23:47:24 +0500

    Layout of th current clicked student page

---
+++ src/Pages/SpecficStu.jsx
@@ -0,0 +1,12 @@
+import SideMenu from "../Components/SideMenu"
+import styles from "./SpecficStu.module.css"
+
+function SpecficStu() {
+  return (
+    <div className="">
+      <SideMenu />
+    </div>
+  )
+}
+
+export default SpecficStu
