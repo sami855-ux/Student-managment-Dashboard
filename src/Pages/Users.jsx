bf698503eefd09ddb14128844820ff593cd72227
sami855-ux <samitale56@outlook.com>
2024-09-20 09:05:13 +0500

    User page is added(blank)

---
+++ src/Pages/Users.jsx
@@ -0,0 +1,13 @@
+import styles from "./Users.module.css"
+import SideMenu from "../Components/SideMenu"
+
+const Users = () => {
+  return (
+    <>
+      <SideMenu />
+      <div className={styles.wrapper}></div>
+    </>
+  )
+}
+
+export default Users
