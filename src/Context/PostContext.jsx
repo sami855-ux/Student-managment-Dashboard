aff68c808d72071651149aa0228f2d72daec4267
sami855-ux <samitale56@outlook.com>
2024-09-20 13:48:32 +0500

    context for post data is modified and added

--- src/Context/PostContext.jsx
+++ src/Context/PostContext.jsx
@@ -1,18 +1,21 @@
-import { createContext, useContext } from "react"
+import { createContext, useContext, useState } from "react"
 import PropTypes from "prop-types"
 
+import postData from "../PostData/posts"
+
 const PostContext = createContext()
+
+const PostContextProvider = ({ children }) => {
+  const [post, setPost] = useState(postData)
+
+  const value = { post }
+  return <PostContext.Provider value={value}>{children}</PostContext.Provider>
+}
 
 //? This only runs on runtime not build time, to run at build time we need to use typescript
 PostContextProvider.propTypes = {
   children: PropTypes.object,
 }
-
-const PostContextProvider = ({ children }) => {
-  const value = {}
-  return <PostContext.Provider value={value}>{children}</PostContext.Provider>
-}
-
 function usePost() {
   const usePost = useContext(PostContext)
 
