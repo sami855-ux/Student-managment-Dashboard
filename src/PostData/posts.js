f30ec3a719979580cac394e4c0c81e56d841b4e0
sami855-ux <samitale56@outlook.com>
2024-09-20 09:17:45 +0500

    Data for posts page

---
+++ src/PostData/posts.js
@@ -0,0 +1,39 @@
+const postData = [
+  {
+    id: "post001",
+    action: "Machine learing: powering intellehent system",
+    date: "2024-09-20T04:10:23.737Z",
+    Author: "Samuel tale",
+  },
+  {
+    id: "post002",
+    action: "Cloud computing: Enabling scalable solutions",
+    date: "2024-09-20T04:10:23.737Z",
+    Author: "john smith",
+  },
+  {
+    id: "post003",
+    action: "Artifical neural network: Mimicking the human brian",
+    date: "2024-09-20T04:10:23.737Z",
+    Author: "James Tylor",
+  },
+  {
+    id: "post004",
+    action: "The rise of artifical intellgence",
+    date: "2024-09-20T04:10:23.737Z",
+    Author: "Jhon Doe",
+  },
+  {
+    id: "post005",
+    action: "5G technology: The next generation of internet",
+    date: "2024-09-20T04:10:23.737Z",
+    Author: "Isabelle smith",
+  },
+  {
+    id: "post006",
+    action: "Qunatum computing: A new era of computing",
+    date: "2024-09-20T04:10:23.737Z",
+    Author: "Jhon Doe",
+  },
+]
+export default postData
